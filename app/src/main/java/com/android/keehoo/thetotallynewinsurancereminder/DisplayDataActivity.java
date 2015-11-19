530037bb7a9cb76fbeeb37485cfd639eb9a61164
KRZYSZTOF KUBICKI <kkubicki2@gmail.com>
2015-11-19 14:09:49 +0100

    OnBackPressed method added to make the user unable to go back to MainActivity by pressing the back button.... work in progress

--- app/src/main/java/com/android/keehoo/thetotallynewinsurancereminder/DisplayDataActivity.java
+++ app/src/main/java/com/android/keehoo/thetotallynewinsurancereminder/DisplayDataActivity.java
@@ -1,7 +1,9 @@
 package com.android.keehoo.thetotallynewinsurancereminder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class DisplayDataActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class DisplayDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_data);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Log.d("Activity B", "         OnBackPressed");
+        Intent i = new Intent(DisplayDataActivity.this, MainActivity.class);
+        i.putExtra("onbackpressed", true);
+        startActivity(i);
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
--- app/src/main/java/com/android/keehoo/thetotallynewinsurancereminder/MainActivity.java
+++ app/src/main/java/com/android/keehoo/thetotallynewinsurancereminder/MainActivity.java
@@ -8,17 +8,60 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
 
     public static final String SHARED_PREFS_NAME = "prefs";
     public static final String SHARED_DATE = "data";
-    public long ustawionaData;
-    public SharedPreferences insurance_date;
+    public long ustwionaDataWMilisekundach;
+
+    public SharedPreferences sharedPreferences;
     private Button sd;
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        sd = (Button) findViewById(R.id.set_date_is);
+        textView = (TextView) findViewById(R.id.status_id);
+
+        sharedPreferences = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+
+        if (sharedPreferences.contains(SHARED_DATE)) {   //Data juz ustawiona i zapisana w sharedPrefs.
+            Intent intent = new Intent(MainActivity.this, DisplayDataActivity.class);
+            intent.putExtra("data", ustwionaDataWMilisekundach);
+            startActivity(intent);
+
+        } else {
+            Toast.makeText(MainActivity.this, "Wybierz date ubezpieczenia", Toast.LENGTH_LONG).show();
+            sd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final DatePicker dp = (DatePicker) findViewById(R.id.dp);
+                    ustwionaDataWMilisekundach = getDateFromDatePicket(dp);
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+                    String ustawionaDataJakoData = formatter.format(new Date(ustwionaDataWMilisekundach));
+                    textView.setText(ustawionaDataJakoData);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putLong(SHARED_DATE, ustwionaDataWMilisekundach);
+                    editor.commit();
+                }
+
+            });
+        }
+
+    }
 
 
     public long getDateFromDatePicket(DatePicker datePicker) {   // chce zeby ta metoda zwracala date z datepickera w milisekundach
@@ -32,26 +75,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final DatePicker dp = (DatePicker) findViewById(R.id.dp);
-
-            sd.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                ustawionaData = getDateFromDatePicket(dp);
-                Intent intent = new Intent(MainActivity.this, DisplayDataActivity.class);
-                intent.putExtra("data", ustawionaData);
-                insurance_date = getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = insurance_date.edit();
-                editor.putLong(SHARED_DATE, ustawionaData);
-                editor.commit();
-                startActivity(intent);
-            }
-        });
-    }
 }
