9f4832630240da28e20a41fdcb26f6e125d91f63
KRZYSZTOF KUBICKI <kkubicki2@gmail.com>
2015-12-10 23:33:25 +0100

    SeekBar added + funcionality for insurance

--- app/src/main/java/com/android/keehoo/thetotallynewinsurancereminder/DisplayDataActivity.java
+++ app/src/main/java/com/android/keehoo/thetotallynewinsurancereminder/DisplayDataActivity.java
@@ -8,22 +8,149 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
+
 
 import net.danlew.android.joda.JodaTimeAndroid;
 
 import org.joda.time.DateTime;
 import org.joda.time.Days;
 
+import java.util.Date;
+
+import butterknife.Bind;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class DisplayDataActivity extends AppCompatActivity {
 
+
     private long dataUbezpieczenieWMilisekundach;
+    private long dataTechnicalWMilisekundach;
     public SharedPreferences sharedPreferences;
     private TextView finalDisplayDays;
-    public static final String SHARED_DATE = "data";
-    private String days;
+    private TextView finalTechnicalDisplayDays;
+    private Button newInsuranceDate;
+    private Button newTechnicalDate;
+    private RadioGroup radiogroup;
+    private RadioGroup radiogroupTech;
+    private SeekBar seekBarTech;
+    private SeekBar seekBarInsurance;
+
+
+    private int okresUbezpieczeniaWmiesiacach;
+    private int okresPrzegladuTechWmiesiacach;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_data);
+        JodaTimeAndroid.init(this);
+        sharedPreferences = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+
+        seekBarInsurance = (SeekBar)findViewById(R.id.seekBar);
+        seekBarTech = (SeekBar)findViewById(R.id.seekBar2);
+        radiogroup = (RadioGroup) findViewById(R.id.radio_group_insurance_id);
+        radiogroupTech = (RadioGroup) findViewById(R.id.radio_group_technical_id);
+        finalDisplayDays = (TextView) findViewById(R.id.days_left_display_id);
+        finalTechnicalDisplayDays = (TextView) findViewById(R.id.days_left_display_technical_id);
+        newInsuranceDate = (Button) findViewById(R.id.new_insurance_date_id);
+        newInsuranceDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeFromSharedPreferences(MainActivity.SHARED_DATE);
+                Log.d("DisplayData", "Usunieto date ubezpieczenia");
+                finalDisplayDays.setText("N/A");
+                if (!sharedPreferences.contains(MainActivity.SHARED_DATE)) {
+                    Intent intent = new Intent(DisplayDataActivity.this, MainActivity.class);
+                    intent.putExtra(MainActivity.TECHNICAL_BUTTON_ENABLED, false);
+                    startActivity(intent);
+                } else {
+                    Log.d("Display", "Wyglada na to, ze nie usunelo sie SHARED_DATE");
+                }
+            }
+        });
+
+
+        newTechnicalDate = (Button) findViewById(R.id.new_technical_date_id);
+        newTechnicalDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeFromSharedPreferences(MainActivity.SHARED_DATE_TECHNICAL);
+                Log.d("DisplayDateAct", " Usunieto Date przegladu technicznego");
+                finalTechnicalDisplayDays.setText("N/A");
+                if (sharedPreferences.contains(MainActivity.SHARED_DATE_TECHNICAL)) {
+                    Log.d("Display", " Wyglada na to, ze shared date sie nie ususnel z kluczaa SHARED_DATE_TECHNICAL");
+                } else {
+                    Intent intent = new Intent(DisplayDataActivity.this, MainActivity.class);
+                    intent.putExtra(MainActivity.INSURANCE_BUTTON_ENABLED, false);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+
+
+        radiogroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                if (checkedId == R.id.twelve_months_insurance_id) {
+                    setOkresUbezpieczeniaWmiesiacach(12);
+                    setInsuranceDisplay(okresUbezpieczeniaWmiesiacach);
+                }
+                if (checkedId == R.id.six_months_insurance_id) {
+                    setOkresUbezpieczeniaWmiesiacach(6);
+                    setInsuranceDisplay(okresUbezpieczeniaWmiesiacach);
+                }
+
+                if (checkedId == R.id.choose_months_insurance_id)
+                    setOkresUbezpieczeniaWmiesiacach(1);
+            }
+        });
+
+        radiogroupTech.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.twelve_months_id) {
+                    setOkresPrzegladuTechWmiesiacach(12);
+                }
+
+                if (checkedId == R.id.six_months_id) {setOkresPrzegladuTechWmiesiacach(6);
+
+                }
+                if (checkedId == R.id.choose_months_id) {
+
+                }
+            }
+        });
+
+        seekBarInsurance.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                setOkresUbezpieczeniaWmiesiacach(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                radiogroup.check(R.id.choose_months_insurance_id);
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                setInsuranceDisplay(okresUbezpieczeniaWmiesiacach);
+
+            }
+        });
+    }
 
 
     @Override
@@ -37,47 +164,37 @@ public class DisplayDataActivity extends AppCompatActivity {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_data);
-        JodaTimeAndroid.init(this);
-
-        sharedPreferences = getSharedPreferences("prefs", Context.MODE_PRIVATE);
-        dataUbezpieczenieWMilisekundach = (sharedPreferences.getLong(SHARED_DATE, -1));
-        finalDisplayDays = (TextView) findViewById(R.id.days_left_display_id);
-
-        DateTime dt = new DateTime(dataUbezpieczenieWMilisekundach);
-        DateTime dtYear = new DateTime(dt.plusYears(1));
-        DateTime dtSixMonths = new DateTime(dt.plusMonths(6)); // unused yet - the funcionality will be added to display insurances that last 6 months or custom amount of days.
+    public String daysBetween(DateTime dateTime, int numberOfMonths) {
+        /**
+         * Zawraca stringa (liczbe dni pomiedzy) parametry: DateTime & ile miesiecy od DateTime
+         */
         DateTime now = new DateTime();
-
-        days = String.valueOf(Days.daysBetween(now.withTimeAtStartOfDay(), dtYear.withTimeAtStartOfDay()).getDays());
-        finalDisplayDays.setText(days);
+        DateTime dataZaIlesTamMiesiecy = dateTime.plusMonths(numberOfMonths);
+        return String.valueOf(Days.daysBetween(now.withTimeAtStartOfDay(), dataZaIlesTamMiesiecy.withTimeAtStartOfDay()).getDays());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
+    public void removeFromSharedPreferences(String key) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove(key).apply();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.ustaw_nowa_date_id) {
-            Log.d("OnOptionsItemSelected", "      WYBRANO: set_data_id");
-            sharedPreferences.edit().remove("data").apply();
-            Log.d("OnOptionsItemSelected", "      Usunieto SHARED PREFERENCES");
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
-            return true;
-        } else if (item.getItemId() == R.id.ustaw_notification) {
-            Intent intent = new Intent(DisplayDataActivity.this, SetNotificationActivity.class);
-            startActivity(intent);
-            return true;
-
-        }
-        return super.onOptionsItemSelected(item);
+    public void setOkresUbezpieczeniaWmiesiacach(int okresUbezpieczeniaWmiesiacach) {
+        this.okresUbezpieczeniaWmiesiacach = okresUbezpieczeniaWmiesiacach;
     }
+
+    public void setOkresPrzegladuTechWmiesiacach(int okresPrzegladuTechWmiesiacach) {
+        this.okresPrzegladuTechWmiesiacach = okresPrzegladuTechWmiesiacach;
+    }
+
+    public void setInsuranceDisplay(int okresUbezpieczeniaWmiesiacach) {
+        dataUbezpieczenieWMilisekundach = (sharedPreferences.getLong(MainActivity.SHARED_DATE, -1));
+        finalDisplayDays.setText(daysBetween(new DateTime(dataUbezpieczenieWMilisekundach), okresUbezpieczeniaWmiesiacach));
+
+    }
+
+    public void setTechnicaDisplay(int okresPrzegladuTechWmiesiacach) {
+        dataTechnicalWMilisekundach = (sharedPreferences.getLong(MainActivity.SHARED_DATE_TECHNICAL, -1));
+        finalTechnicalDisplayDays.setText(daysBetween(new DateTime(dataTechnicalWMilisekundach), okresPrzegladuTechWmiesiacach));
+    }
+
 }
--- app/src/main/java/com/android/keehoo/thetotallynewinsurancereminder/MainActivity.java
+++ app/src/main/java/com/android/keehoo/thetotallynewinsurancereminder/MainActivity.java
@@ -16,24 +16,125 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.joda.time.DateTime;
+import org.w3c.dom.Text;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String BROADCAST_ACTION =
-            BuildConfig.APPLICATION_ID + ".BROADCAST_ACTION";
     public static final String SHARED_PREFS_NAME = "prefs";
     public static final String SHARED_DATE = "data";
+    public static final String SHARED_DATE_TECHNICAL = "data_technical";
+
+    public static final String INSURANCE_BUTTON_ENABLED = "false";
+    public static final String TECHNICAL_BUTTON_ENABLED = "false";
+
     public long ustwionaDataWMilisekundach;
-
-
     public SharedPreferences sharedPreferences;
-    //private Button sd;
-    //private TextView textView;
+
+    Button insButton;  // default - visible only in package
+    Button techButton;  //default accessor - visible only in package - I hope :)
+
+
+
+    @Bind(R.id.obecna_data_ubezpieczenia)
+    protected TextView obecnaDataUbezpieczenia;
+
+    @Bind(R.id.obecna_data_technical)
+    protected  TextView obecnaDataTechnical;
+
+    @OnClick(R.id.set_insurance_date_id)
+    public void onSetInsuranceDateClick() {
+        final DatePicker dp = (DatePicker) findViewById(R.id.dp);
+        ustwionaDataWMilisekundach = getDateFromDatePicket(dp);
+        saveInSharedPreferences(SHARED_DATE, ustwionaDataWMilisekundach);
+        Toast.makeText(MainActivity.this, "Data ubezpieczenia ustawiona na " + ustwionaDataWMilisekundach, Toast.LENGTH_LONG).show();
+        obecnaDataUbezpieczenia.setText("Obecna data ubezpieczenia " + dateText(new DateTime(sharedPreferences.getLong("data", -1))));
+        ustwionaDataWMilisekundach = 0;
+    }
+
+    @OnClick(R.id.set_technical_date_id)
+    public void onSetTechnicalDateClick() {
+        final DatePicker dp = (DatePicker) findViewById(R.id.dp);
+        ustwionaDataWMilisekundach = getDateFromDatePicket(dp);
+        saveInSharedPreferences(SHARED_DATE_TECHNICAL, ustwionaDataWMilisekundach);
+        Toast.makeText(MainActivity.this, "Data przegladu technicznego ustawiona na " + ustwionaDataWMilisekundach, Toast.LENGTH_LONG).show();
+        obecnaDataTechnical.setText("Obecna data przegladu " + dateText(new DateTime(sharedPreferences.getLong("data_technical", -1))));
+        ustwionaDataWMilisekundach = 0;
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        Log.d("MainActivity", "start");
+        insButton = (Button)findViewById(R.id.set_insurance_date_id);
+        Log.d("MainActivity", "insButton");
+        techButton = (Button)findViewById(R.id.set_technical_date_id);
+        Log.d("MainActivity", "techButton");
+
+        if (getIntent().getExtras() != null) {
+            insButton.setEnabled(getIntent().getExtras().getBoolean(INSURANCE_BUTTON_ENABLED, false));
+            Log.d("MainActivity", "getBoolean INSURANCE BUTTON ENABLED");
+            techButton.setEnabled(getIntent().getExtras().getBoolean(TECHNICAL_BUTTON_ENABLED, false));
+            Log.d("MainActivity", "getBoolean TECH BUTTON ENABLED");
+        }
+        sharedPreferences = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+
+        if (sharedPreferences.contains(SHARED_DATE) && sharedPreferences.contains(SHARED_DATE_TECHNICAL)) {
+            //Data juz ustawiona i zapisana w sharedPrefs. Otwieramy nowe okno z wywietlaczem
+            Log.d("MainActivity - OnCreate", "    Shared prefs contain both - insurance date and technical check date");
+            Intent intent = new Intent(MainActivity.this, DisplayDataActivity.class);
+            startActivity(intent);
+        }
+        if (sharedPreferences.contains(SHARED_DATE_TECHNICAL)){
+            Log.d("MainActivity - OnCreate", "    Shared prefs contain technical check date only");
+            insButton.setEnabled(true);
+            Toast.makeText(MainActivity.this, "Proszę wybrać date rozpoczęcia ubezpieczenia", Toast.LENGTH_SHORT).show();
+        }
+        if (sharedPreferences.contains(SHARED_DATE)){
+            Log.d("MainActivity - OnCreate", "    Shared prefs contain insurance date only ");
+            Toast.makeText(MainActivity.this, "Proszę wybrać datę wykonania przeglądu technicznego", Toast.LENGTH_SHORT).show();
+            techButton.setEnabled(true);
+        }
+        else
+        {
+            Log.d("SharedPreferences", "      Shared Preferences does not contain SHARED_DAT tag");
+            Toast.makeText(MainActivity.this, "Wybierz date ubezpieczenia", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public long getDateFromDatePicket(DatePicker datePicker) {
+        /**
+         * getDateFromDatePicket - zwraca wybrana date z Date Picker'a - returns Calendar in miliseconds
+         */
+        // chce zeby ta metoda zwracala date z datepickera w milisekundach
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year = datePicker.getYear();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+        return calendar.getTimeInMillis();
+    }
+
+    public void saveInSharedPreferences(String key, long dataWmilisekundach) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putLong(key, dataWmilisekundach).apply();
+    }
+
 
 
     @Override
@@ -45,73 +146,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.ustaw_date) {
-            final DatePicker dp = (DatePicker) findViewById(R.id.dp);
-            ustwionaDataWMilisekundach = getDateFromDatePicket(dp);
-            sharedPreferences = getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putLong(SHARED_DATE, ustwionaDataWMilisekundach).apply();
-            Log.d("SharedPreferences", "Zmienna ustawionaDataWMilisekundach o wartosci "
-                    + ustwionaDataWMilisekundach + " zostala zapisana w SharedPreferences pod tagiem SHARED_DATE");
             Intent intent = new Intent(MainActivity.this, DisplayDataActivity.class);
             startActivity(intent);
-            Log.d("OnOptionsItemSelected", "   Wybrano date i uruchomiono Activity Display Date");
             finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        Log.d("OnCreate-Activity Main", "            OnCreate w Main Activity");
-
-        sharedPreferences = getSharedPreferences("prefs", Context.MODE_PRIVATE);
-
-        if (sharedPreferences.contains(SHARED_DATE)) {
-            //Data juz ustawiona i zapisana w sharedPrefs. Otwieramy nowe okno z wywietlaczem
-            Log.d("SharedPreferences", "         Shared Preferences contains SHARED_DATA tag");
-            Intent intent = new Intent(MainActivity.this, DisplayDataActivity.class);
-            intent.putExtra("data", ustwionaDataWMilisekundach);
-            startActivity(intent);
-            finish();
-
-
-        } else {
-            Log.d("SharedPreferences", "      Shared Preferences does not contain SHARED_DAT tag");
-            Toast.makeText(MainActivity.this, "Wybierz date ubezpieczenia", Toast.LENGTH_LONG).show();
-        }
+    public String dateText(DateTime dateTime) {
+/**
+ * zmienia date z JodaTime na text - tylko dzien, miesiac i rok
+ */
+        int rok = dateTime.getYear();
+        int miesiac = dateTime.getMonthOfYear();
+        int dzien = dateTime.getDayOfMonth();
+        return new String(dzien + " " + miesiac + " " + rok );
     }
-
-    /**
-     * getDateFromDatePicket - zwraca wybrana date z Date Picker'a - returns Calendar in miliseconds
-     */
-
-    public long getDateFromDatePicket(DatePicker datePicker) {
-        // chce zeby ta metoda zwracala date z datepickera w milisekundach
-        int day = datePicker.getDayOfMonth();
-        int month = datePicker.getMonth();
-        int year = datePicker.getYear();
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, day);
-        return calendar.getTimeInMillis();
-    }
-
-   /* private void scheduleNotification(String text, int seconds) {
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        //Log.d("MainActivity", "  alarmManager - getSystemService(Context.ALARM_SERVICE");
-
-        Intent broadcast = new Intent(BROADCAST_ACTION);
-        broadcast.putExtra(NotificationService.EXTRA_NOTIFICATION_TEXT, text);
-        //Log.d("MainActivity", "Nowy intent o nazwie broadcast (INTENT(BROADCAST_ACTION) put Extra pod zmienna text= EXTRA_NOTIFICATION_TEXT");
-
-        PendingIntent alarmAction = PendingIntent.getBroadcast(this, text.hashCode(), broadcast, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        //Log.d("MainActivity", " PendingIntent alarmAction z parametrami this, hashCode, broadcast i pendingIntent.FLAG_UPDATE_CURRENT");
-        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (seconds * 1000), alarmAction);
-        //Log.d("MainActivity", "setAlarmManager z parametrami AlarmManager.RTC_WAKEUP, currentTime + sekundy ustawione wczesniej, alarmAction... alarmAction to jest pendingItentn powyzej");
-    }*/
 }
