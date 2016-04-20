1c3bdb1263eae49100d37820fd6d8a4537051d30
Chris Kubicky <kkubicki2@gmail.com>
2016-04-20 11:36:04 +0200

    Adapter completed

--- app/src/main/java/com/multivonex/keehoo/thetotallynewinsurancereminder/CarAdapter.java
+++ app/src/main/java/com/multivonex/keehoo/thetotallynewinsurancereminder/CarAdapter.java
@@ -10,18 +10,17 @@ import android.widget.TextView;
 
 import com.android.keehoo.thetotallynewinsurancereminder.R;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
 
 import cn.iwgang.countdownview.CountdownView;
 
-/**
- * Created by keehoo on 19.04.2016.
- */
 public class CarAdapter extends RecyclerView.Adapter<CarAdapter.CarViewHolder> {
 
     private List<Car> dane;
     private LayoutInflater layoutInflater;
-    private OnItemClickListener onItemClickListener;
+    private OnItemClickListener onClickListener;
 
     public CarAdapter(Context context, List<Car> dane) {
         this.dane = dane; // podlaczenie danych z argumentów konstruktora do danych (listy samochodów)
@@ -29,7 +28,7 @@ public class CarAdapter extends RecyclerView.Adapter<CarAdapter.CarViewHolder> {
     }
 
     public void setOnItemClickListener(OnItemClickListener onClickListener) {
-        this.onItemClickListener = onClickListener;
+        this.onClickListener = onClickListener;
 
 
     }
@@ -44,13 +43,32 @@ public class CarAdapter extends RecyclerView.Adapter<CarAdapter.CarViewHolder> {
     public void onBindViewHolder(CarViewHolder holder, int position) {
         Car car = dane.get(position);  // przypisujemy obiektowi car referencje z listy dane o pozycji position
 
+        // 2. Uzupełnij widok (holder) pobranymi danymi
+        holder.carModel.setText(car.getMarkaSamochodu());
+        // W metode setText gdy chcemy wcisnac wartosc INT, musimy to robic przez Integer.toString()
+
+        holder.carRegistrationNumber.setText(car.getNumer_Rejestracyjny());
+        holder.dataOfCarInsurance.setText("Tutaj nalezy wstawic longa przepisanego na date i na stringa z data ubezpieczenia");
+        holder.dateOfCarTechnicalCheck.setText("Tutaj data przegladu");
+        holder.timeTillNextTechnicalCheck.start(1000);   // TODO: tutaj wstawic longa z casem do konca obowiazywania ubezpieczenia.
+        holder.timeTillTheEndOfInsurance.start(1000);    // TODO: tutaj wstawic longa z czasem do konca przegladu w milisekundach.
+        holder.currentPosition = position;
+        holder.currentObject = car;
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return dane.size();
+
     }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 
 
     public static class CarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -65,14 +83,32 @@ public class CarAdapter extends RecyclerView.Adapter<CarAdapter.CarViewHolder> {
         public int currentPosition;
         public Car currentObject;
         public CarAdapter adapter;
-    }
-
-    public CarViewHolder(View itemView, CarAdapter adapter) {
-
-        super(itemView);
-        this.adapter = adapter;
 
 
+        public CarViewHolder(View itemView, CarAdapter adapter) {
+
+            super(itemView);
+            this.adapter = adapter;
+            carModel = (TextView) itemView.findViewById(R.id.carName_id);
+            carRegistrationNumber = (TextView) itemView.findViewById(R.id.numerRejestracyjny_id);
+            dataOfCarInsurance = (TextView) itemView.findViewById(R.id.insuranceDateInTheItemView);
+            dateOfCarTechnicalCheck = (TextView) itemView.findViewById(R.id.technicalDateInTheItemView);
+            timeTillTheEndOfInsurance = (CountdownView) itemView.findViewById(R.id.insuranceCountDownNumber);
+            timeTillNextTechnicalCheck = (CountdownView) itemView.findViewById(R.id.technicalCountDownNumber);
+
+            itemView.setOnClickListener(this);
+
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            // 1. Najpierw sprawdzam czy w adapterze istnieje onClickListener
+
+            if (adapter.onClickListener != null) {
+                adapter.onClickListener.onClick(currentPosition, currentObject);
+            }
+        }
     }
 
 
